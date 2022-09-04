4a8713da42cfc1e9d53bb147e5687cc2e7b82739
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-09-05 00:06:15 +0700

    Membuat relasi Has One antara tabel users dengan  profiles

--- server/models/profile.go
+++ server/models/profile.go
@@ -3,13 +3,24 @@ package models
 import "time"
 
 type Profile struct {
-	ID       int    `json:"id" gorm:"primary_key:auto_increment"`
-	Img      string `json:"img" gorm:"type: varchar(255)"`
-	Phone    string `json:"phone" gorm:"type: varchar(255)"`
-	Address  string `json:"address" gorm:"type: text"`
-	PostCode string `json:"postCode" gorm:"column:post_code type:varchar(255)"`
-	UserID   int    `json:"userID" gorm:"column: user_id"`
-	// User      UserProfileRel `json:"user"`
+	ID        int       `json:"id" gorm:"primary_key:auto_increment"`
+	Img       string    `json:"img" gorm:"type: varchar(255)"`
+	Phone     string    `json:"phone" gorm:"type: varchar(255)"`
+	Address   string    `json:"address" gorm:"type: text"`
+	PostCode  string    `json:"postCode" gorm:"type:varchar(255); column:post_code"`
+	UserID    int       `json:"userID" gorm:"column:user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ProfileResponseRelation struct {
+	Phone    string `json:"phone"`
+	Img      string `json:"img"`
+	Address  string `json:"address"`
+	PostCode string `json:"postCode" gorm:"column:post_code"`
+	UserID   int    `json:"-"`
+}
+
+func (ProfileResponseRelation) TableName() string {
+	return "profiles"
 }
--- server/models/user.go
+++ server/models/user.go
@@ -3,13 +3,24 @@ package models
 import "time"
 
 type User struct {
-	ID       int    `json:"id" gorm:"primary_key:auto_increment"`
-	FullName string `json:"fullName" gorm:"column:full_name type: varchar(255)"`
-	Email    string `json:"email" gorm:"type: varchar(255)"`
-	Password string `json:"password" gorm:"type: varchar(255)"`
-	Status   string `json:"status"`
-	// Profile     ProfileResponse      `json:"profile" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID       int                     `json:"id" gorm:"primary_key:auto_increment"`
+	FullName string                  `json:"fullName" gorm:"column:full_name type: varchar(255)"`
+	Email    string                  `json:"email" gorm:"type: varchar(255)"`
+	Password string                  `json:"password" gorm:"type: varchar(255)"`
+	Status   string                  `json:"status"`
+	Profile  ProfileResponseRelation `json:"profile"`
 	// Transaction []TransactionUserRel `json:"transaction"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
+}
+
+type UserResponseRelation struct {
+	FullName string `json:"fullName"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Status   string `json:"status"`
+}
+
+func (UserResponseRelation) TableName() string {
+	return "profiles"
 }
